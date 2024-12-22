f1185c05c23f04b1d34c37a74f1bc4988541aa0d
ftiercelin <francois.tiercelin@hpe.com>
2024-12-22 15:41:17 +0000

    lint

--- utils/src/main/java/org/owasp/dependencycheck/utils/CredentialHelper.java
+++ utils/src/main/java/org/owasp/dependencycheck/utils/CredentialHelper.java
@@ -13,40 +13,40 @@ import org.apache.hc.client5.http.auth.StandardAuthScheme;
 import org.apache.hc.client5.http.auth.UsernamePasswordCredentials;
 
 public class CredentialHelper {
-	public static Credentials getCredentials(String messageScope, String theUser, char[] thePass, char[] theToken, char[] theAuth) throws InvalidSettingException {
+    public static Credentials getCredentials(String messageScope, String theUser, char[] thePass, char[] theToken, char[] theAuth) throws InvalidSettingException {
         Credentials _creds = null;
-	     
+         
         // create a basic auth for user and password, if provided
         if (theUser != null && !theUser.isBlank()) {
-        	if(thePass == null || thePass.length == 0)
-        		throw new InvalidSettingException("No password provided for user " + theUser + " for " + messageScope);
-       		try {
-       			_creds = new UsernamePasswordCredentials(theUser, thePass);
-       		} catch (Exception e) {
-            	throw new InvalidSettingException("Invalid authentication provided: invalid user or password");       			        	
-       		}
+            if(thePass == null || thePass.length == 0)
+                throw new InvalidSettingException("No password provided for user " + theUser + " for " + messageScope);
+               try {
+                   _creds = new UsernamePasswordCredentials(theUser, thePass);
+               } catch (Exception e) {
+                throw new InvalidSettingException("Invalid authentication provided: invalid user or password");                               
+               }
         }
         
         // create a bearer token, if provided, takes precedence over user/password/token
-		if (theToken != null && theToken.length > 0) {
-			_creds = new BearerToken(new String(theToken));
-		}
+        if (theToken != null && theToken.length > 0) {
+            _creds = new BearerToken(new String(theToken));
+        }
         
         // if an auth has been passed, takes precedence over user/password/token
         if (theAuth != null && theAuth.length > 0) {
-        	if(startsWith(theAuth,StandardAuthScheme.BASIC)) {
-        		_creds = getBasicCredentialsFromAuthHeader(theAuth);
-        	} else if(startsWith(theAuth, StandardAuthScheme.BEARER)) {
-        		_creds = getBearerCredentialsFromAuthHeader(theAuth);
-        	} else
-            	throw new InvalidSettingException("Invalid authentication provided: unknown authentication scheme. "
-            			+ "Supported authentication schemes: " 
-            			+ StandardAuthScheme.BASIC + " and " + StandardAuthScheme.BEARER);       			        	
+            if(startsWith(theAuth,StandardAuthScheme.BASIC)) {
+                _creds = getBasicCredentialsFromAuthHeader(theAuth);
+            } else if(startsWith(theAuth, StandardAuthScheme.BEARER)) {
+                _creds = getBearerCredentialsFromAuthHeader(theAuth);
+            } else
+                throw new InvalidSettingException("Invalid authentication provided: unknown authentication scheme. "
+                        + "Supported authentication schemes: " 
+                        + StandardAuthScheme.BASIC + " and " + StandardAuthScheme.BEARER);                               
         }
-		return _creds;
-	}
+        return _creds;
+    }
 
-	/**
+    /**
      * Create a bearer token credentials from the auth header
      * @param authHeader
      * @return credentials
@@ -54,139 +54,139 @@ public class CredentialHelper {
      */
     protected static Credentials getBearerCredentialsFromAuthHeader(char[] authHeader) throws InvalidSettingException {
         if (authHeader == null || authHeader.length == 0)
-        	throw new InvalidSettingException("empty authentication header");
-		try {
-    		// get token
-	        String token = new String(authHeader);
-	        if(!token.startsWith(StandardAuthScheme.BEARER + " "))
-	        	throw new InvalidSettingException("auth header should start with [" + StandardAuthScheme.BEARER + " ]");
-	        token = token.replaceAll("^" + Pattern.quote(StandardAuthScheme.BEARER),"").trim();
-	        if(token.isBlank())
-	        	throw new InvalidSettingException("empty bearer token");
-			return new BearerToken(token);
-		} catch (Exception e) {
-        	throw new InvalidSettingException("Invalid authentication provided: " + e.getMessage());       			        	
-		}
-	}
+            throw new InvalidSettingException("empty authentication header");
+        try {
+            // get token
+            String token = new String(authHeader);
+            if(!token.startsWith(StandardAuthScheme.BEARER + " "))
+                throw new InvalidSettingException("auth header should start with [" + StandardAuthScheme.BEARER + " ]");
+            token = token.replaceAll("^" + Pattern.quote(StandardAuthScheme.BEARER),"").trim();
+            if(token.isBlank())
+                throw new InvalidSettingException("empty bearer token");
+            return new BearerToken(token);
+        } catch (Exception e) {
+            throw new InvalidSettingException("Invalid authentication provided: " + e.getMessage());                               
+        }
+    }
 
-	/**
+    /**
      * Create a basic credentials from the basic auth header
      * @param authHeader
      * @return credentials
      * @throws InvalidSettingException
      */
-	protected static Credentials getBasicCredentialsFromAuthHeader(char[] authHeader) throws InvalidSettingException {
+    protected static Credentials getBasicCredentialsFromAuthHeader(char[] authHeader) throws InvalidSettingException {
         if (authHeader == null || authHeader.length == 0)
-        	throw new InvalidSettingException("empty authentication header");
-		try {
-    		// decode B64 to get user and password
-			String user = getBasicUser(authHeader, StandardAuthScheme.BASIC.length()+1); 
-			return new UsernamePasswordCredentials(
-    				user,
-    				getBasicPassword(authHeader, StandardAuthScheme.BASIC.length()+1)
-    				);
-		} catch (Exception e) {
-        	throw new InvalidSettingException("Invalid authentication provided: " + e.getMessage());       			        	
-		}
-	}
+            throw new InvalidSettingException("empty authentication header");
+        try {
+            // decode B64 to get user and password
+            String user = getBasicUser(authHeader, StandardAuthScheme.BASIC.length() + 1); 
+            return new UsernamePasswordCredentials(
+                    user,
+                    getBasicPassword(authHeader, StandardAuthScheme.BASIC.length() + 1)
+                    );
+        } catch (Exception e) {
+            throw new InvalidSettingException("Invalid authentication provided: " + e.getMessage());                               
+        }
+    }
 
 
 
-	/**
-	 * copy a char array into an array of bytes without using a String
-	 * @param in
-	 * @param start where to start in the array if chars
-	 * @return byte array
-	 * @throws InvalidSettingException 
-	 */
-	protected static byte[] toBytes(char[] in, int start) throws InvalidSettingException {
+    /**
+     * copy a char array into an array of bytes without using a String
+     * @param in
+     * @param start where to start in the array if chars
+     * @return byte array
+     * @throws InvalidSettingException 
+     */
+    protected static byte[] toBytes(char[] in, int start) throws InvalidSettingException {
         if (in == null || in.length == 0)
-        	throw new InvalidSettingException("Invalid authentication provided");
+            throw new InvalidSettingException("Invalid authentication provided");
         if(start >= in.length)
-        	throw new InvalidSettingException("Invalid authentication provided");
+            throw new InvalidSettingException("Invalid authentication provided");
         char[] chars = new char[in.length - start];
         for(int i = start; i < in.length; i++) {
-        	chars[i-start] = in[i];
+            chars[i-start] = in[i];
         }
         CharBuffer charBuffer = CharBuffer.wrap(chars);
         ByteBuffer byteBuffer = StandardCharsets.UTF_8.encode(charBuffer);
         byte[] bytes = Arrays.copyOfRange(byteBuffer.array(),
-        		byteBuffer.position(), byteBuffer.limit());
+                byteBuffer.position(), byteBuffer.limit());
         Arrays.fill(byteBuffer.array(), (byte) 0); // clear sensitive data
         return bytes;
-	}
-	
-	/**
-	 * get the user out of a b64 string (Basic auth)
-	 * @param in
-	 * @param start
-	 * @return
-	 * @throws InvalidSettingException
-	 */
-	protected static String getBasicUser(char[] in, int start) throws InvalidSettingException {
+    }
+    
+    /**
+     * get the user out of a b64 string (Basic auth)
+     * @param in
+     * @param start
+     * @return
+     * @throws InvalidSettingException
+     */
+    protected static String getBasicUser(char[] in, int start) throws InvalidSettingException {
         if (in == null || in.length == 0)
-        	throw new InvalidSettingException("Invalid authentication string");
+            throw new InvalidSettingException("Invalid authentication string");
         if(start >= in.length)
-        	throw new InvalidSettingException("authentication string too short");
+            throw new InvalidSettingException("authentication string too short");
         byte[] src = toBytes(in, start);
         if(src == null || src.length ==0)
-        	return null;
+            return null;
         byte[] decoded = Base64.getDecoder().decode(src);
         String user = "";
         for(int i = 0; i < decoded.length; i++) {
-        	if(decoded[i]!=':') user+=(char)decoded[i];
-        	else return user;
+            if(decoded[i]!=':') user += (char) decoded[i];
+            else return user;
         }
-    	throw new InvalidSettingException("unable to find user");
-	}
-	
-	/**
-	 * get the password out of a B64 string  (Basic auth)
-	 * @param in
-	 * @param start
-	 * @return
-	 * @throws InvalidSettingException
-	 */
-	protected static char[] getBasicPassword(char[] in, int start) throws InvalidSettingException {
+        throw new InvalidSettingException("unable to find user");
+    }
+    
+    /**
+     * get the password out of a B64 string  (Basic auth)
+     * @param in
+     * @param start
+     * @return
+     * @throws InvalidSettingException
+     */
+    protected static char[] getBasicPassword(char[] in, int start) throws InvalidSettingException {
         if (in == null || in.length == 0)
-        	throw new InvalidSettingException("Invalid authentication string");
+            throw new InvalidSettingException("Invalid authentication string");
         if(start >= in.length)
-        	throw new InvalidSettingException("authentication string too short");
+            throw new InvalidSettingException("authentication string too short");
         byte[] src = toBytes(in, start);
         if(src == null || src.length ==0)
-        	return null;
+            return null;
         byte[] decoded = Base64.getDecoder().decode(src);
         start = 0;
         for(int i = 0; i < decoded.length && start == 0; i++) {
-        	if(decoded[i]==':') start = i + 1;
+            if(decoded[i] == ':') start = i + 1;
         }
         if(start == 0 || start >= decoded.length)
-        	throw new InvalidSettingException("unable to find password");
+            throw new InvalidSettingException("unable to find password");
         
         char[] password = new char[decoded.length - start];
         for(int i = start; i < decoded.length; i++) {
-        	password[i - start] = (char) (decoded[i] & 0xFF) ;
+            password[i - start] = (char) (decoded[i] & 0xFF) ;
         }
         return password;
-	}
+    }
 
-	/**
-	 * Utility function to perform startsWith on a char array
-	 * @param in
-	 * @param start
-	 * @return
-	 */
-	protected static boolean startsWith(char[] in, String start) {
+    /**
+     * Utility function to perform startsWith on a char array
+     * @param in
+     * @param start
+     * @return
+     */
+    protected static boolean startsWith(char[] in, String start) {
         if (in == null || in.length == 0)
-        	return false;
+            return false;
         if (start == null || start.isBlank())
-        	return false;
+            return false;
         if(start.length() > in.length)
-        	return false;
-        for(int i=0;i<start.length();i++) {
-        	if(start.charAt(i) != in[i]) return false;
+            return false;
+        for(int i = 0; i < start.length(); i++) {
+            if(start.charAt(i) != in[i]) return false;
         }
         return true;
-	}
+    }
 
 }
